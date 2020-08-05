c7936db26a31c5d6773e2da1783d5a23a2fb94ae
meenachinmay <chinmayanand896@gmail.com>
2020-08-05 14:59:41 +0530

    form active

--- client/src/Pages/Login.js
+++ client/src/Pages/Login.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const show = () => {
+        console.log(email);
+    }
+
     return (
         <div className="flex-shrink-0 mx-auto w-1/5">
             <div className="p-2 mt-32 rounded bg-white shadow-lg">
@@ -8,19 +16,31 @@ const Login = () => {
                     <p className="text-center text-2xl uppercase text-gray-500">
                         Sign In
                     </p>
-                    <label className="text-gray-500">
+                    <label className="text-gray-600">
                         Email:
                     </label>
-                    <input type="email" className="p-2 focus:outline-none bg-gray-100 border-1 border-solid rounded hover:bg-gray-300 focus:bg-gray-300 text-gray-700" required/>
+                    <input 
+                        type="email" 
+                        className="p-2 focus:outline-none bg-gray-100 border-1 border-solid rounded hover:bg-gray-300 focus:bg-gray-300 text-gray-700" 
+                        value={email}
+                        onChange={(e) => {setEmail(e.target.value)}}
+                        required
+                    />
 
-                    <label className="text-gray-500 mt-3">
+                    <label className="text-gray-600 mt-3">
                         Password:
                     </label>
-                    <input type="password" className="p-2 focus:outline-none bg-gray-100 border-1 border-solid rounded hover:bg-gray-300 focus:bg-gray-300 text-gray-700" required/>
+                    <input 
+                        type="password" 
+                        className="p-2 focus:outline-none bg-gray-100 border-1 border-solid rounded hover:bg-gray-300 focus:bg-gray-300 text-gray-700" 
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
                 </div>
             </div>
             <div className="text-center mt-6">
-                <button onClick={() => alert('clicked')} className="text-white bg-indigo-500 px-6 py-3 rounded focus:outline-none hover:bg-indigo-600 shadow-xl">Sign In</button>
+                <button onClick={show} className="text-white bg-indigo-500 px-6 py-3 rounded focus:outline-none hover:bg-indigo-600 shadow-xl">Sign In</button>
             </div>
         </div>
     )
